2831d3e8dfe2d076ce5a7664a10c8188b73b7120
Rociomartos29 <137335471+Rociomartos29@users.noreply.github.com>
2023-07-24 22:42:46 +0200

    f:validar numero

--- romanos.py
+++ romanos.py
@@ -1,6 +1,10 @@
+
 def num_romano(numero):
-    if type(numero) != int:
+    # if type(numero) != int:
+    if not isinstance(numero, int):
         return f"Error: debes introducir un numero entero ({numero})"
+    if not (0 < numero < 4000):
+        return f"Error: el numero debe ser entre 0 y 3999({numero})"
     elif type(numero) == int:
         return "TODO: convertir a romano"
 
@@ -10,7 +14,8 @@ print(num_romano(56.1))
 print(num_romano("lo que quieras"))
 print(num_romano([]))
 print(num_romano({}))
-
+print(num_romano(1))
+print(num_romano(3999))
 
 # if not isinstamce(numero, int):
 #       return f"Error": debes introducir un numero entero ({numero})
