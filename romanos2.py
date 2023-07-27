45d0354c062e133905a2c3e0b9425bce44cf3d3e
Rociomartos29 <137335471+Rociomartos29@users.noreply.github.com>
2023-07-27 21:07:22 +0200

    MEJORADO

--- romanos2.py
+++ romanos2.py
@@ -1,9 +1,24 @@
+''' validacion_de_tres = comprobar_repe_masdetres(romano_a_entero)
+if validacion_de_tres:
+    raise Exception(
+        f'ERROR'
+    ) '''
+digitos_romanos = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
 
+
+'''def comprobar_repe_masdetres(nromano):
+    anterior = ''
+    contador = 0
+    _out = False
+    for index in nromano:
+        if index == anterior:
+            contador += 1
+        anterior = index
+    return contador == 3'''
 
 
 def romano_a_entero(romano):
 
-    digitos_romanos = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
     valores = [1, 5, 10, 50, 100, 500, 1000]
     digitos_romanos = {
         'I': 1,
@@ -15,39 +30,35 @@ def romano_a_entero(romano):
         'M': 1000
     }
     if not isinstance(romano, str):
-        return 'ERROR: tiene que ser un numero romano en formato cadena de texto'
-    
-    
-  
+        raise TypeError(
+            'ERROR: tiene que ser un numero romano en formato cadena de texto')
 
     resultado = 0
     anterior = 0
     for letra in romano:
         if letra not in digitos_romanos:
-            return f'ERROR: {letra} no es un digito romano válido'
-        
-
+            raise ValueError('ERROR: {letra} no es un digito romano válido')
 
         actual = digitos_romanos[letra]
 
-        if anterior < actual: 
-            #Comprobar que la resta es posible
-            #el orden de magnitud  no es mayor de uno
-            if anterior < 0 and len(str(actual)) > len (str(anterior)) > 1:
-                return 'ERROR: resta no posible'
-            #deshacer la suma que hemos hecho antes
-            resultado = resultado - anterior
-            #sumar el valor actual, pero restando el anterior
-            resultado = resultado + (actual - anterior)
-        else:
-            resultado = resultado + actual
+    if anterior < actual:
+        # Comprobar que la resta es posible
+        # el orden de magnitud  no es mayor de uno
+        if anterior < 0 and len(str(actual)) > len(str(anterior)) > 1:
+            raise ValueError(f'ERROR: resta no posible')
+        # deshacer la suma que hemos hecho antes
+        resultado = resultado - anterior
+        # sumar el valor actual, pero restando el anterior
+        resultado = resultado + (actual - anterior)
+    else:
+        resultado = resultado + actual
 
+    anterior = actual
 
-        anterior = actual
     return resultado
 
+
 errores = ['A', '', 3, ['X', 'X', 'I']]
-pruebas = [ 'I', 'MCXXIII', 'VIII', 'LVI', 'IV', 'DCIV']
+pruebas = ['I', 'MCXXIII', 'VIII', 'LVI', 'IV', 'DCIV']
 for valor in pruebas:
     print(romano_a_entero(valor))
-
