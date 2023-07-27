35b7c5c982f2014e5aa896b54f1b0497e5d7ba30
Rociomartos29 <137335471+Rociomartos29@users.noreply.github.com>
2023-07-27 21:07:34 +0200

    TEST 1

---
+++ test.py
@@ -0,0 +1,17 @@
+import unittest
+from romanos2 import romano_a_entero
+
+
+class romanpstest(unittest.TestCase):
+
+    def test_unidades(self):
+        self.assertEqual(romano_a_entero('I'), 1)
+        self.assertEqual(romano_a_entero('V'), 5)
+        self.assertEqual(romano_a_entero('X'), 10)
+        self.assertEqual(romano_a_entero('L'), 50)
+        self.assertEqual(romano_a_entero('C'), 100)
+        self.assertEqual(romano_a_entero('D'), 500)
+        self.assertEqual(romano_a_entero('M'), 1000)
+
+
+unittest.main()
